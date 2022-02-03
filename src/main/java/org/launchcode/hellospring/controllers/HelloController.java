a9c5cd3de241061ecc5185d9742bd05a675ef58e
Jenhsiang Hsieh (Wren) <Wren@Wrens-MacBook-Pro-14-2021.local>
2022-02-03 14:13:24 -0600

    finished EX

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -49,4 +49,49 @@ public class HelloController {
                 "</html>";
     }
 
+    @RequestMapping(path = "byLang", method = {RequestMethod.GET, RequestMethod.POST})
+    public static String helloWithPathParamByLang(@RequestParam String name, @RequestParam String lang) {
+        if (lang.equals("en")) {
+            return String.format("Hello, %s!", name);
+        } else if (lang.equals("fr")) {
+            return String.format("Bonjour, %s!", name);
+        } else if (lang.equals("cn")) {
+            return "<html>" +
+                    "<body style = 'color:green'>" +
+                    "哈囉，" + name + " !" +
+                    "</body>" +
+                    "<html>";
+            //return String.format("哈囉，%s！", name);
+        } else if (lang.equals("sp")) {
+            return String.format("¡Hola, %s!", name);
+        } else if (lang.equals("jp")) {
+            return String.format("こんにちは，%s！", name);
+        } else {
+            return "Hello, " + name + "!";
+        }
+
+    }
+
+
+
+    @GetMapping("form/lang")
+    public String helloFormByLang() {
+        return "<html> " +
+                "<body>" +
+                "<form action = '/hello/byLang' method = 'post'>" +
+                    "<input type = 'text' name = 'name'>" +
+                    "<select id = 'lang' name = 'lang'>" +
+                        "<option value = 'en'>English</option>" +
+                        "<option value = 'fr'>Français</option>" +
+                        "<option value = 'cn'>中文</option>" +
+                        "<option value = 'sp'>Español</option>" +
+                        "<option value = 'jp'>日本語</option>" +
+                    "</select>" +
+                    "<input type = 'submit' value = 'Greet Me!'>" +
+                "</form>" +
+                "</body>" +
+                "</html>";
+    }
+
+
 }
